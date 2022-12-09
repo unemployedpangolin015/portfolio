b355310c5127766af074ecd87a1d769aec734ddf
unemployedpangolin015 <115036973+unemployedpangolin015@users.noreply.github.com>
2022-12-08 21:33:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ColorModeScript } from '@chakra-ui/react';
+import { HashRouter } from 'react-router-dom';
+
+import theme from './theme';
+
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/800.css'
+import '@fontsource/raleway/400.css'
+import '@fontsource/raleway/700.css'
+import '@fontsource/work-sans/400.css'
+import '@fontsource/work-sans/700.css'
+import '@fontsource/work-sans/800.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
